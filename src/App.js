7d3f5ac45401e33830f8c699a33c03e5b24e8296
Cre8tion <ljl1997@gmail.com>
2020-03-15 22:49:29 +0800

    Blank Design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="h1">Lee Jia Le | SUTD</h1>
+      <h2 className="h2">Work-In-Progress</h2>
     </div>
   );
 }
