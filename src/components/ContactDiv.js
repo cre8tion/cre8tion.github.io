ae5bf07e9085cddcacc495a75a9e6b524688bed5
Cre8tion <ljl1997@gmail.com>
2021-01-19 14:55:49 +0800

    Added Labels

--- src/components/ContactDiv.js
+++ src/components/ContactDiv.js
@@ -6,13 +6,13 @@ function ContactDiv() {
     return (
         <div className="horizontal">
             <div>
-                <SocialIcon url="http://linkedin.com/in/leejiale" />
+                <SocialIcon url="http://linkedin.com/in/leejiale" label="LinkedIn"  />
             </div>
             <div>
-                <SocialIcon url="https://github.com/cre8tion" />
+                <SocialIcon url="https://github.com/cre8tion" label="Github"  />
             </div>
             <div>
-                <SocialIcon network="email" url="mailto:ljl1997@gmail.com"/>
+                <SocialIcon network="email" url="mailto:ljl1997@gmail.com" label="Email Address"/>
             </div>
         </div>
     );
